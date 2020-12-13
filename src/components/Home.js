82229a5badcaff40577b53d59286fcaee5b022e3
afornagieri <fornagieri.artur@gmail.com>
2020-12-13 17:26:47 -0300

    feat(devices):create dashboard

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,45 @@
 import React from "react";
+import { Container, Grid } from "semantic-ui-react";
+import Charts from './Charts';
 
-import { Container } from "semantic-ui-react";
-import {} from "semantic-ui-react";
+import {
+  PieConfig,
+  DoughnutConfig,
+  PolarConfig,
+  RadarConfig,
+  LineConfig, } from '../data/mock';
 
 export default function Home() {
   return (
-    <Container fluid className={"main-container"}>
-      Hello World
+    <Container className={"main-container"}>
+      <Grid>
+        <Grid.Row stretched>
+            <Grid.Column width={7}>
+              {Charts(PieConfig)}
+            </Grid.Column>
+
+            <Grid.Column width={7}>
+              {Charts(DoughnutConfig)}
+            </Grid.Column>
+        </Grid.Row>
+
+        <Grid.Row stretched>
+          <Grid.Column width={7}>
+            {Charts(PolarConfig)}
+          </Grid.Column>
+
+          <Grid.Column width={7}>
+            {Charts(RadarConfig)}
+          </Grid.Column>
+        </Grid.Row>
+
+        <Grid.Row stretched>
+          <Grid.Column width={14}>
+            {Charts(LineConfig)}
+          </Grid.Column>
+        </Grid.Row>
+
+      </Grid>
     </Container>
   );
 }
--- src/provider/index.js
+++ src/provider/index.js
@@ -1,14 +1,17 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
 // Context
 import MyContext from "../context/MyContext";
 
 export default function Provider({ children }) {
   const [visible, setVisible] = useState(false);
+  const [ChartInstance, setChartInstance] = useState(null);
 
   const context = {
     visible,
     setVisible,
+    ChartInstance,
+    setChartInstance,
   };
 
   return <MyContext.Provider value={context}>{children}</MyContext.Provider>;
