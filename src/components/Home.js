7e8d88c99835bd306920d9ee43a95bc605e28360
afornagieri <fornagieri.artur@gmail.com>
2020-12-13 03:24:08 -0300

    feat(home):create home

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Sidebar from './Sidebar';
+
+import { Container } from 'semantic-ui-react'
+
+export default function Home() {
+  return(
+    <Container>
+      <Sidebar />
+    </Container>
+  )
+}
