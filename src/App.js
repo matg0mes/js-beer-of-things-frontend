e5e0ea74c49665ba2807ab188b53583de42d208a
MatheusD3V <matheus.gomes@pixter.com.br>
2020-12-13 14:37:30 -0300

    fix(app): take off default archives

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 // Components
-import Home from './components/Home';
+import Home from "./components/Home";
+import Sidebar from "./components/Sidebar";
 
 // Semantic UI
-import 'semantic-ui-css/semantic.min.css';
+import "semantic-ui-css/semantic.min.css";
+
+//Styles
+import "./";
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/" component={ Home }/>
+        <Sidebar />
+        <Route exact path="/" component={Home} />
         {/* <Route exact path="/home" component={ Home } /> */}
       </Switch>
     </BrowserRouter>
